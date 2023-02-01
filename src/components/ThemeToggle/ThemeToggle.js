b18cd959a1170a4917865b905cd28e5e4e0d8ffc
Artur C <araghast94@icloud.com>
2023-02-01 18:19:12 +0100

    added dark mode functionality

--- src/components/ThemeToggle/ThemeToggle.js
+++ src/components/ThemeToggle/ThemeToggle.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './ThemeToggle.css'
 
 export function ThemeToggle() {
+    const [ isDark, setIsDark ] = useState(false);
+
+    const root = document.querySelector(':root')
+
+    useEffect(() => {
+        if(isDark){
+            root.classList.add('dark')
+        } else {
+            root.classList.remove('dark')
+        }
+    }, [isDark, root.classList])
+
     return (
         <div className='toggle'>
-            <input type='checkbox' id='darkmode-toggle' />
+            <input type='checkbox' id='darkmode-toggle' onChange={() => setIsDark(!isDark)} />
             <label for='darkmode-toggle' id='toggle-label' />
         </div>
     )
