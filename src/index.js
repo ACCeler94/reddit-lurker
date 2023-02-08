c9ed9557b44650ccab4aff4a7ceade6c2d9b11bf
Artur C <araghast94@icloud.com>
2023-02-08 14:08:27 +0100

    added RouterProvider

--- src/index.js
+++ src/index.js
@@ -5,14 +5,22 @@ import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
