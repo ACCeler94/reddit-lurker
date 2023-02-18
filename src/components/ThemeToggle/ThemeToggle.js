f15e4b686f2c750b585b8698b9c72c47cfd9978a
Artur C <araghast94@icloud.com>
2023-02-18 14:37:20 +0100

    temporary theme toggle solution

--- src/components/ThemeToggle/ThemeToggle.js
+++ src/components/ThemeToggle/ThemeToggle.js
@@ -2,21 +2,42 @@ import React, { useEffect, useState } from 'react';
 import './ThemeToggle.css'
 
 export function ThemeToggle() {
-    const [ isDark, setIsDark ] = useState(false);
+    const [ isDark, setIsDark ] = useState(true);
 
     const root = document.querySelector(':root')
 
+    const storeUserSetPreference = (pref) => {
+        localStorage.setItem("isDark", pref);
+        };
+
+    
+    const getUserSetPreference = () => {
+        return localStorage.getItem("isDark");
+            };
+
+
+    const changeThemeHandler = () => {
+        setIsDark(!isDark)
+        storeUserSetPreference(isDark)
+    }
+
+    
+
     useEffect(() => {
-        if(isDark){
+        let userPref = getUserSetPreference()
+        console.log(userPref)
+
+        if(!isDark){
             root.classList.add('dark')
         } else {
             root.classList.remove('dark')
         }
-    }, [isDark, root.classList])
+    }, [isDark, root.classList ])
+
 
     return (
         <div className='toggle'>
-            <input type='checkbox' id='darkmode-toggle' onChange={() => setIsDark(!isDark)} />
+            <input type='checkbox' id='darkmode-toggle' onChange={changeThemeHandler} defaultChecked={!isDark} />
             <label for='darkmode-toggle' id='toggle-label' />
         </div>
     )
