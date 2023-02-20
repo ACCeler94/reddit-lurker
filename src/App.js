5d95f9a9347a3c0e02817e6061c8ad029d9eaaa3
Artur C <araghast94@icloud.com>
2023-02-20 13:53:02 +0100

    fixed eslint error

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import './App.css';
 import { Header } from "./components/Header/Header";
 import { TopList } from './components/TopList/TopList';
-import { Posts } from './components/Posts/Posts';
-import { LoadMoreButton } from './components/LoadMoreButton/LoadMoreButton';
 import { Outlet } from 'react-router-dom';
 
 
